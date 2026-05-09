8456c09c55c1d812d1ee9e496d4e0132af7a9a57
umg239 <umang25csu249@ncuindia.edu>
2026-05-09 12:40:23 +0530

    Create Q20.cpp

---
+++ Q20.cpp
@@ -0,0 +1,15 @@
+/*A network security system generates prime numbers in a range for encryption key pools. Implement a
+solution to find all prime numbers within a given range.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        cout << "\n";
+        for (int j = 1; j <= 5; j++) {
+            cout << j << " ";
+        }
+    }
+    return 0;
+} 
